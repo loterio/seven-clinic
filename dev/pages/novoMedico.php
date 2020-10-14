8905e853259648b74b831a4231e58ffa0983f162
Henrique Borges dos Santos <henrique.10agr@gmail.com>
2020-10-14 18:24:22 -0300

    Criação dos formulários

---
+++ dev/pages/novoMedico.php
@@ -0,0 +1,26 @@
+<button class="fechar" onclick="fechaFormulario()"><img src="../assets/img/fechar.svg" alt=""></button>
+<h2 class="titulo">Médico</h2>
+<form action="" autocomplete="off">
+  <div class="input input-grande">
+    <label for="nome">Nome</label>
+    <input type="text" id="nome">
+  </div>
+
+  <div class="input input-pequeno">
+    <label for="crm">CRM</label>
+    <input type="text" id="crm">
+  </div>
+  <div class="input input-pequeno">
+    <label for="telefone">Telefone</label>
+    <input type="text" id="telefone">
+  </div>
+
+  <div class="input input-grande">
+    <label for="especializacao">Expecialização</label>
+    <input type="text" id="especializacao">
+  </div>
+
+  <div class="botao-submit input-grande">
+    <input type="submit" value="Adicionar">
+  </div>
+</form>
---
+++ dev/pages/novoPaciente.php
@@ -0,0 +1,53 @@
+<button class="fechar" onclick="fechaFormulario()"><img src="../assets/img/fechar.svg" alt=""></button>
+<h2 class="titulo">Paciente</h2>
+<form action="" autocomplete="off">
+  <div class="input input-grande">
+    <label for="nome">Nome</label>
+    <input type="text" id="nome">
+  </div>
+
+  <div class="input input-pequeno">
+    <label for="cpf">CPF</label>
+    <input type="text" id="cpf">
+  </div>
+  <div class="input input-pequeno">
+    <label for="altura">Altura</label>
+    <input type="text" id="altura">
+  </div>
+
+  <div class="input input-pequeno">
+    <label for="peso">Peso</label>
+    <input type="text" id="peso">
+  </div>
+  <div class="input input-pequeno">
+    <label for="data-nascimento">Data de Nascimento</label>
+    <input type="date" id="data-nascimento">
+  </div>
+
+  <div class="input input-pequeno">
+    <label for="email">Email</label>
+    <input type="email" id="email">
+  </div>
+  <div class="input input-pequeno">
+    <label for="telefone">Telefone</label>
+    <input type="text" id="telefone">
+  </div>
+
+  <div class="input input-pequeno">
+    <label for="endereco">Endereço</label>
+    <input type="text" id="endereco">
+  </div>
+  <div class="input input-pequeno">
+    <label for="cidade">Cidade</label>
+    <input type="text" id="cidade">
+  </div>
+
+  <div class="input input-grande">
+    <label for="observacoes">Observações</label>
+    <textarea id="observacoes"></textarea>
+  </div>
+
+  <div class="botao-submit input-grande">
+    <input type="submit" value="Adicionar">
+  </div>
+</form>
