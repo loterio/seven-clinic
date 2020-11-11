59bd881ec2227bd9584eb44eccb6ad3ba005d11c
feleonhardt <cabanha.fl@gmail.com>
2020-11-11 15:28:40 -0300

    Criação da funcao alteraConsulta

--- dev/assets/consulta.class.php
+++ dev/assets/consulta.class.php
@@ -26,88 +26,166 @@ class Consulta
         $this->id_paciente = $id_paciente;
         $this->id_medico = $id_medico;
         $this->estado = $estado;
-   }
-
-   public function setAddConsulta(){
-    $countConsultasIdInicio= getQnt('consultas', $this->id_user);
-    $countConsultasMedico= $this->getVerificaConsultaMedico();
-    $countConsultasPaciente= $this->getVerificaConsultaPaciente();
-    $msg = '';
+    }
     
-    if ($countConsultasMedico == 0) {
-        if ($countConsultasPaciente == 0) {
-            $sql = 'INSERT INTO consultas(id_user, data_consulta, hora_inicio, hora_fim, valor, descricao, id_paciente, id_medico, estado) values(:id_user, :data_consulta, :hora_inicio, :hora_fim, :valor, :descricao, :id_paciente, :id_medico, :estado);';
-            $stmt = preparaComando($sql);
-            $bind = array(
-                ':id_user' => $this->id_user,
-                ':data_consulta' => $this->data_consulta,
-                ':hora_inicio' => $this->hora_inicio,
-                ':hora_fim' => $this->hora_fim,
-                ':valor' => $this->valor,
-                ':descricao' => $this->descricao,
-                ':id_paciente' => $this->id_paciente,
-                ':id_medico' => $this->id_medico,
-                ':estado' => $this->estado
-            );
-            $stmt = bindExecute($stmt, $bind);
-            
-            $countConsultasIdFim= getQnt('consultas', $this->id_user);
-            
-            if ($countConsultasIdInicio < $countConsultasIdFim) {
-                // echo ('Consulta cadastrada com sucesso!');
-                $msg = 'Consulta cadastrada com sucesso!';
-                // $_SESSION['msg'] = "Consulta cadastrada com sucesso!";
-                // header('location:agendamento.php?status=OK'); // Sucesso
+    public function setAddConsulta(){
+        $countConsultasIdInicio= getQnt('consultas', $this->id_user);
+        $countConsultasMedico= $this->getVerificaConsultaMedico(FALSE);
+        $countConsultasPaciente= $this->getVerificaConsultaPaciente(FALSE);
+        $msg = '';
+        
+        if ($countConsultasMedico == 0) {
+            if ($countConsultasPaciente == 0) {
+                $sql = 'INSERT INTO consultas(id_user, data_consulta, hora_inicio, hora_fim, valor, descricao, id_paciente, id_medico, estado) values(:id_user, :data_consulta, :hora_inicio, :hora_fim, :valor, :descricao, :id_paciente, :id_medico, :estado);';
+                $stmt = preparaComando($sql);
+                $bind = array(
+                    ':id_user' => $this->id_user,
+                    ':data_consulta' => $this->data_consulta,
+                    ':hora_inicio' => $this->hora_inicio,
+                    ':hora_fim' => $this->hora_fim,
+                    ':valor' => $this->valor,
+                    ':descricao' => $this->descricao,
+                    ':id_paciente' => $this->id_paciente,
+                    ':id_medico' => $this->id_medico,
+                    ':estado' => $this->estado
+                );
+                $stmt = bindExecute($stmt, $bind);
+                
+                $countConsultasIdFim= getQnt('consultas', $this->id_user);
+                
+                if ($countConsultasIdInicio < $countConsultasIdFim) {
+                    // echo ('Consulta cadastrada com sucesso!');
+                    $msg = 'Consulta cadastrada com sucesso!';
+                    // $_SESSION['msg'] = "Consulta cadastrada com sucesso!";
+                    // header('location:agendamento.php?status=OK'); // Sucesso
+                }else {
+                    // echo ('Erro ao adicionar consulta!');
+                    $msg = 'Erro ao adicionar consulta!';
+                    // $_SESSION['msg'] = "Erro ao adicionar consulta!";
+                    // header('location:agendamento.php?status=ERRO');
+                }
             }else {
-                // echo ('Erro ao adicionar consulta!');
-                $msg = 'Erro ao adicionar consulta!';
-                // $_SESSION['msg'] = "Erro ao adicionar consulta!";
+                // echo ('Este horário já possui outra consulta agendada para este paciente!');
+                $msg = 'Este horário já possui outra consulta agendada para este paciente!';
+                // $_SESSION['msg'] = "Este horário já possui outra consulta agendada para este paciente!";
                 // header('location:agendamento.php?status=ERRO');
             }
         }else {
-            // echo ('Este horário já possui outra consulta agendada para este paciente!');
-            $msg = 'Este horário já possui outra consulta agendada para este paciente!';
-            // $_SESSION['msg'] = "Este horário já possui outra consulta agendada para este paciente!";
+            // echo ('Este horário já possui outra consulta agendada para este médico!');
+            $msg = 'Este horário já possui outra consulta agendada para este médico!';
+            // $_SESSION['msg'] = "Este horário já possui outra consulta agendada para este médico!";
             // header('location:agendamento.php?status=ERRO');
-        }
-    }else {
-        // echo ('Este horário já possui outra consulta agendada para este médico!');
-        $msg = 'Este horário já possui outra consulta agendada para este médico!';
-        // $_SESSION['msg'] = "Este horário já possui outra consulta agendada para este médico!";
-        // header('location:agendamento.php?status=ERRO');
-    }   
-    return $msg;
-   }
+        }   
+        return $msg;
+    }
+    
+    public function getVerificaConsultaMedico($op){
 
-   public function getVerificaConsultaMedico(){
-       $sql = 'SELECT COUNT(*) AS qntConsulta FROM consultas WHERE id_user = :id_user and id_medico = :id_medico and data_consulta = :data_consulta and ((hora_inicio <= :hora_inicio and hora_fim > :hora_inicio) or (hora_inicio >= :hora_inicio and hora_inicio < :hora_fim and hora_fim > :hora_inicio));';
-       $stmt = preparaComando($sql);
-       $bind = array(
-           ':id_user' => $this->id_user,
-           ':id_medico' => $this->id_medico,
-           ':hora_inicio' => $this->hora_inicio,
-           ':hora_fim' => $this->hora_fim,
-           ':data_consulta' => $this->data_consulta,
-        //    ':hora_inicio' => $this->hora_inicio,
-        //    ':hora_fim' => $this->hora_fim
-        );
+        if ($op == TRUE) {
+            $sql = 'SELECT COUNT(*) AS qntConsulta FROM consultas WHERE id_user = :id_user and id_medico = :id_medico and data_consulta = :data_consulta and id_consulta != :id_consulta and ((hora_inicio <= :hora_inicio and hora_fim > :hora_inicio) or (hora_inicio >= :hora_inicio and hora_inicio < :hora_fim and hora_fim > :hora_inicio));';
+            $stmt = preparaComando($sql);
+            $bind = array(
+                ':id_user' => $this->id_user,
+                ':id_medico' => $this->id_medico,
+                ':id_consulta' => $this->id_consulta,
+                ':hora_inicio' => $this->hora_inicio,
+                ':hora_fim' => $this->hora_fim,
+                ':data_consulta' => $this->data_consulta
+            );
+        }else {
+            $sql = 'SELECT COUNT(*) AS qntConsulta FROM consultas WHERE id_user = :id_user and id_medico = :id_medico and data_consulta = :data_consulta and ((hora_inicio <= :hora_inicio and hora_fim > :hora_inicio) or (hora_inicio >= :hora_inicio and hora_inicio < :hora_fim and hora_fim > :hora_inicio));';
+            $stmt = preparaComando($sql);
+            $bind = array(
+                ':id_user' => $this->id_user,
+                ':id_medico' => $this->id_medico,
+                ':hora_inicio' => $this->hora_inicio,
+                ':hora_fim' => $this->hora_fim,
+                ':data_consulta' => $this->data_consulta
+            );
+        }
         $stmt = bindExecute($stmt, $bind);
         return $stmt->fetch(PDO::FETCH_ASSOC)['qntConsulta'];
     }
-    public function getVerificaConsultaPaciente(){
-        $sql = 'SELECT COUNT(*) AS qntConsulta FROM consultas WHERE id_user = :id_user and id_paciente = :id_paciente and data_consulta = :data_consulta and ((hora_inicio <= :hora_inicio and hora_fim > :hora_inicio) or (hora_inicio >= :hora_inicio and hora_inicio < :hora_fim and hora_fim > :hora_inicio));';
-        $stmt = preparaComando($sql);
-        $bind = array(
-            ':id_user' => $this->id_user,
-            ':id_paciente' => $this->id_paciente,
-            ':hora_inicio' => $this->hora_inicio,
-            ':hora_fim' => $this->hora_fim,
-            ':data_consulta' => $this->data_consulta,
-         //    ':hora_inicio' => $this->hora_inicio,
-         //    ':hora_fim' => $this->hora_fim
-         );
-         $stmt = bindExecute($stmt, $bind);
-         return $stmt->fetch(PDO::FETCH_ASSOC)['qntConsulta'];
-     }
+    
+    public function getVerificaConsultaPaciente($op){
+        if ($op == TRUE) {
+            $sql = 'SELECT COUNT(*) AS qntConsulta FROM consultas WHERE id_user = :id_user and id_paciente = :id_paciente and data_consulta = :data_consulta and id_consulta != :id_consulta and ((hora_inicio <= :hora_inicio and hora_fim > :hora_inicio) or (hora_inicio >= :hora_inicio and hora_inicio < :hora_fim and hora_fim > :hora_inicio));';
+            $stmt = preparaComando($sql);
+            $bind = array(
+                ':id_user' => $this->id_user,
+                ':id_paciente' => $this->id_paciente,
+                ':id_consulta' => $this->id_consulta,
+                ':hora_inicio' => $this->hora_inicio,
+                ':hora_fim' => $this->hora_fim,
+                ':data_consulta' => $this->data_consulta
+            );
+        }else {
+            $sql = 'SELECT COUNT(*) AS qntConsulta FROM consultas WHERE id_user = :id_user and id_paciente = :id_paciente and data_consulta = :data_consulta and ((hora_inicio <= :hora_inicio and hora_fim > :hora_inicio) or (hora_inicio >= :hora_inicio and hora_inicio < :hora_fim and hora_fim > :hora_inicio));';
+            $stmt = preparaComando($sql);
+            $bind = array(
+                ':id_user' => $this->id_user,
+                ':id_paciente' => $this->id_paciente,
+                ':hora_inicio' => $this->hora_inicio,
+                ':hora_fim' => $this->hora_fim,
+                ':data_consulta' => $this->data_consulta
+            );
+        }
+            $stmt = bindExecute($stmt, $bind);
+        return $stmt->fetch(PDO::FETCH_ASSOC)['qntConsulta'];
+    }
+
+    public function setIdConsulta($id){
+        $this->id_consulta = $id;
+    }
+    
+    public function setAlteraConsulta($id_consulta){
+        $this->setIdConsulta($id_consulta);
+        $countConsultasMedico= $this->getVerificaConsultaMedico(TRUE);
+        $countConsultasPaciente= $this->getVerificaConsultaPaciente(TRUE);
+        $msg = '';
+        
+        if ($countConsultasMedico == 0) {
+            if ($countConsultasPaciente == 0) {
+                $sql = 'UPDATE consultas SET data_consulta = :data_consulta, hora_inicio = :hora_inicio, hora_fim = :hora_fim, valor = :valor, descricao = :descricao, id_paciente = :id_paciente, id_medico = :id_medico, estado = :estado WHERE id_user = :id_user AND id_consulta = :id_consulta;';
+                $stmt = preparaComando($sql);
+                $bind = array(
+                    ':id_user' => $this->id_user,
+                    ':id_consulta' => $this->id_consulta,
+                    ':data_consulta' => $this->data_consulta,
+                    ':hora_inicio' => $this->hora_inicio,
+                    ':hora_fim' => $this->hora_fim,
+                    ':valor' => $this->valor,
+                    ':descricao' => $this->descricao,
+                    ':id_paciente' => $this->id_paciente,
+                    ':id_medico' => $this->id_medico,
+                    ':estado' => $this->estado
+                );
+                $stmt = bindExecute($stmt, $bind);
+                
+                if( $stmt->rowCount() > 0 ) {
+                    // echo ('Consulta atualizada com sucesso!');
+                    $msg = 'Consulta atualizada com sucesso!';
+                    // $_SESSION['msg'] = "Consulta atualizada com sucesso!";
+                    // header('location:agendamento.php?status=OK');
+                } else {
+                    // echo ('Não foi possível atualizar a consulta!');
+                    $msg = 'Não foi possível atualizar a consulta!';
+                    // $_SESSION['msg'] = "Não foi possível atualizar a consulta!";
+                    // header('location:agendamento.php?status=ERRO');
+                }
+            }else {
+                // echo ('Este horário já possui outra consulta agendada para este paciente!');
+                $msg = 'Este horário já possui outra consulta agendada para este paciente!';
+                // $_SESSION['msg'] = "Este horário já possui outra consulta agendada para este paciente!";
+                // header('location:agendamento.php?status=ERRO');
+            }
+        }else {
+            // echo ('Este horário já possui outra consulta agendada para este médico!');
+            $msg = 'Este horário já possui outra consulta agendada para este médico!';
+            // $_SESSION['msg'] = "Este horário já possui outra consulta agendada para este médico!";
+            // header('location:agendamento.php?status=ERRO');
+        }   
+        return $msg;
+    }
 }
 ?>
--- dev/assets/teste.php
+++ dev/assets/teste.php
@@ -6,8 +6,13 @@ require_once('medico.class.php');
 require_once('paciente.class.php');
 require_once('consulta.class.php');
 require_once('usuario.class.php');
+
+
+
 // EXEMPLO -->> $medico = new Medico(id_user, CRM, 'nome', 'telefone', 'especializacao');
 $medico = new Medico(1, 112, 'Felipe CLASSE 12', '44444444', 'nada');
+
+
 // EXEMPLO -->> echo $medico->setAddMedico();
 // echo $medico->setAddMedico();
 
@@ -15,6 +20,7 @@ $medico = new Medico(1, 112, 'Felipe CLASSE 12', '44444444', 'nada');
 // Médico cadastrado com sucesso!
 // Erro ao adicionar médico!
 // Este CRM já está cadastrado!
+
 
 // EXEMPLO -->> echo $medico->setAlteraMedico(id_medico);
 // echo $medico->setAlteraMedico(12);
@@ -24,8 +30,12 @@ $medico = new Medico(1, 112, 'Felipe CLASSE 12', '44444444', 'nada');
 // Não foi possível atualizar o médico!
 // Este CRM já está cadastrado!
 
+
+
 // EXEMPLO -->> $paciente = new Paciente(id_user, 'nome', 'CPF', altura, peso, 'data_nascimento', 'email', 'telefone', 'endereço', 'cidade', 'observacoes');
 $paciente = new Paciente(1, 'Uelinton teske', '87744', 1, 110, '2002-09-26', 'ueliNton.teskE09@gmail.com', '1234567890', 'Rua tiradentes, 69', 'braço do trombudo', 'Sinusite');
+
+
 // EXEMPLO -->> echo $paciente->setAddPaciente();
 // echo $paciente->setAddPaciente();
 
@@ -33,6 +43,7 @@ $paciente = new Paciente(1, 'Uelinton teske', '87744', 1, 110, '2002-09-26', 'ue
 // Paciente cadastrado com sucesso!
 // Erro ao adicionar paciente!
 // Este CPF já está cadastrado!
+
 
 // EXEMPLO  -->> echo $paciente->setAlteraPaciente(id_user);
 // echo $paciente->setAlteraPaciente(11);
@@ -44,7 +55,9 @@ $paciente = new Paciente(1, 'Uelinton teske', '87744', 1, 110, '2002-09-26', 'ue
 
 
 // EXEMPLO -->> $consulta = new Consulta(id_user, 'data_consulta', 'hora_inicio', 'hora_fim', valor, 'observaçoes', id_paciente, id_medico, estado);
-$consulta = new Consulta(1, '2020-11-12', '07:30:00', '07:45:00', 120, 'ok', 13, 2, 0);
+$consulta = new Consulta(1, '2020-11-12', '06:00:00', '08:30:00', 120, 'ok', 2, 2, 0);
+
+
 // EXEMPLO -->> echo $consulta->setAddConsulta();
 // echo $consulta->setAddConsulta();
 
@@ -55,8 +68,20 @@ $consulta = new Consulta(1, '2020-11-12', '07:30:00', '07:45:00', 120, 'ok', 13,
 // Este horário já possui outra consulta agendada para este médico!
 
 
+// EXEMPLO -->> echo $consulta->setAlteraConsulta(id_consulta);
+// echo $consulta->setAlteraConsulta(1);
+
+// ---------RETORNOS---------
+// Consulta atualizada com sucesso!
+// Não foi possível atualizar a consulta!
+// Este horário já possui outra consulta agendada para este paciente!
+// Este horário já possui outra consulta agendada para este médico!
+
+
 // EXEMPLO -->> $usuario = new Usuario('email');
 $usuario = new Usuario('jao123@gmail.com');
+
+
 // EXEMPLO -->> echo $usuario->setAlteraUsuario(id_user, 'nome', 'senha', 'confSenha')
 // echo $usuario->setAlteraUsuario(1, 'jao', 'jao1234', 'jao1234')
 
