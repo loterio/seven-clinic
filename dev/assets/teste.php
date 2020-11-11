056d842b07a40b0dbfb23f971755a0f477af9ecb
feleonhardt <cabanha.fl@gmail.com>
2020-11-11 10:09:03 -0300

    pequena alteracao na teste.php com echo's

--- dev/assets/teste.php
+++ dev/assets/teste.php
@@ -6,7 +6,7 @@ require_once('medico.class.php');
 require_once('paciente.class.php');
 require_once('consulta.class.php');
 $medico = new Medico(1, 112, 'Felipe CLASSE 12', '44444444', 'nada');
-// $medico->setAddMedico();
+// echo $medico->setAddMedico();
 echo $medico->setAlteraMedico(12);
 
 $paciente = new Paciente(1, 'Uelinton teske', '87744', 1, 60, '2002-09-26', 'ueliNton.teskE09@gmail.com', '1234567890', 'Rua tiradentes, 69', 'braço do trombudo', 'nenhuma');
