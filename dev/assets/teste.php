3d324f859f90ce89974bb593fbd90d2d3d143765
feleonhardt <cabanha.fl@gmail.com>
2020-11-11 10:06:11 -0300

    teste da alteraMedico

--- dev/assets/teste.php
+++ dev/assets/teste.php
@@ -5,9 +5,9 @@ iniciaSession();
 require_once('medico.class.php');
 require_once('paciente.class.php');
 require_once('consulta.class.php');
-$medico = new Medico(1, 16, 'Felipe CLASSE 12', '34353434', 'nada');
+$medico = new Medico(1, 112, 'Felipe CLASSE 12', '44444444', 'nada');
 // $medico->setAddMedico();
-$medico->setAlteraMedico(12);
+echo $medico->setAlteraMedico(12);
 
 $paciente = new Paciente(1, 'Uelinton teske', '87744', 1, 60, '2002-09-26', 'ueliNton.teskE09@gmail.com', '1234567890', 'Rua tiradentes, 69', 'braço do trombudo', 'nenhuma');
 $paciente->setAddPaciente();
