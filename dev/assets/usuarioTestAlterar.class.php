d1bc6f2c7872aba691c2d386694bc51f455c948e
dudamartini <martiniduda2002@gmail.com>
2020-11-11 22:47:59 -0300

    adicionado usuarioTestAlterar

---
+++ dev/assets/usuarioTestAlterar.class.php
@@ -0,0 +1,17 @@
+<?php
+use PHPUnit\Framework\TestCase;
+require_once "usuario.class.php";
+
+class UsuatioTesteAlterar extends TestCase 
+{
+    public function testUsuarioAlterar() {
+      $usuario = new Usuario("henrique.10@gmail.com");
+      $this->assertContains($usuario->setAlteraUsuario(1, 'Henrique Borges', '123456', '123456'),"Usuário atualizado com sucesso!");
+    }
+
+    public function testUsuarioAlterarComSenhaInvalidas() {
+      $usuario = new Usuario("henrique.10@gmail.com");
+      $this->assertContains($usuario->setAlteraUsuario(1, 'Henrique Borges', '125321', '106456'),"A senha e a confirmação de senha não coincidem!");
+    }
+}
+?>
