a8d3a56561f69083d251a11958a76e839f8cd4a7
dudamartini <martiniduda2002@gmail.com>
2020-11-16 13:43:04 -0300

    Classe Alterar adicionado

--- dev/assets/usuarioTestAlterar.class.php
+++ dev/assets/usuarioTestAlterar.class.php
@@ -6,12 +6,18 @@ class UsuatioTesteAlterar extends TestCase
 {
     public function testUsuarioAlterar() {
       $usuario = new Usuario("henrique.10@gmail.com");
-      $this->assertContains($usuario->setAlteraUsuario(1, 'Henrique Borges', '123456', '123456'),"Usuário atualizado com sucesso!");
+      $this->assertContains($usuario->setAlteraUsuario(1, 
+      'Henrique Borges', '123456', '123456'),"Usuário atualizado com sucesso!");
     }
 
     public function testUsuarioAlterarComSenhaInvalidas() {
       $usuario = new Usuario("henrique.10@gmail.com");
-      $this->assertContains($usuario->setAlteraUsuario(1, 'Henrique Borges', '125321', '106456'),"A senha e a confirmação de senha não coincidem!");
+      $this->assertContains($usuario->setAlteraUsuario(1, 
+      'Henrique Borges', '125321', '106456'),"A senha e a confirmação de senha não coincidem!");
     }
 }
 ?>
+
+
+
+
