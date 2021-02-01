b1dfd629a40f6acb05dd59e3be19becc04348dae
Henrique Borges dos Santos <henrique.10agr@gmail.com>
2021-02-01 17:41:29 -0300

    procedimento que retorna relatorio

--- dev/assets/conf/SevenClinic-BD.sql
+++ dev/assets/conf/SevenClinic-BD.sql
@@ -60,6 +60,31 @@ FOREIGN KEY (id_user) REFERENCES usuarios(id),
 FOREIGN KEY (id_paciente) REFERENCES pacientes(id_paciente),
 FOREIGN KEY (id_medico) REFERENCES medicos(id_medico)
 );
+
+DELIMITER $$
+	CREATE PROCEDURE relatorio(IN data_inicial DATE, IN data_final DATE, IN id_user INT)
+    BEGIN
+		SELECT 
+			M.nome 			AS NOME_MEDICO, 
+			P.nome 			AS NOME_PACIENTE, 
+			C.data_consulta AS DATA_CONSULTA, 
+			C.hora_inicio 	AS HORA_INICIO_CONSULTA, 
+			C.hora_fim 		AS HORA_FIM_CONSULTA, 
+			C.valor 		AS VALOR_CONSULTA
+		  FROM medicos AS M, consultas AS c, pacientes AS p
+		 WHERE M.id_medico  = C.id_medico 
+		   AND P.id_paciente = C.id_paciente
+		   AND C.id_user = id_user
+		   AND C.data_consulta >= data_inicial
+		   AND C.data_consulta <= data_final;
+    END $$
+$$
+DELIMITER ;
+
+CALL relatorio('2021-02-01', '2021-02-01', 1);
+
+
+
 -- select SUM(valor) as valorTotal from consultas where id_medico = 1 AND id_user = 1 AND estado = 0 AND data_consulta >= '2020-10-14' AND data_consulta <= '2020-11-13';
 -- select * from consultas where data_consulta >= '2020-10-13' AND data_consulta <= '2020-11-11' order by data_consulta;
 
